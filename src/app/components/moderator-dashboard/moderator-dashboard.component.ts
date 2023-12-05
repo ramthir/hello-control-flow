7c2c763bbd71b52091331a79dd9d71694b42b0c2
Ramesh Thiruchelvam <ramesh@limarktech.com>
2023-12-05 23:23:58 +0530

    feat: dashboard components

---
+++ src/app/components/moderator-dashboard/moderator-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moderator-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './moderator-dashboard.component.html',
+  styleUrl: './moderator-dashboard.component.scss'
+})
+export class ModeratorDashboardComponent {
+
+}
---
+++ src/app/components/user-dashboard/user-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-dashboard.component.html',
+  styleUrl: './user-dashboard.component.scss'
+})
+export class UserDashboardComponent {
+
+}
